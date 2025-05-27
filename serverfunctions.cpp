190a68d352aaf9dec1791118ed13c0ddcd3ad306
Juli <kuprianovajuliya@gmail.com>
2025-05-27 13:49:02 +0300

    Добавлен функционал: Sha-1, внедрение сообщения в музыкальный файл

--- serverfunctions.cpp
+++ serverfunctions.cpp
@@ -1,5 +1,6 @@
 #include <serverfunctions.h>
 
+
 QString AUTHMESSAGE = "<auth> - Command to authenticate user in the system\r\nCommon syntax: <auth> <login> <pasword>\r\n\r\n";
 QString REGMESSAGE = "<reg> - Command to create new user in the system\r\nCommon syntax: <reg> <login> <password> <email>\r\n\r\n";
 QString STATMESSAGE = "<lookall> - Command to view all stat in the system\r\nCommon syntax: <lookall> \r\n\r\n";
@@ -58,14 +59,105 @@ QByteArray vigenereCipher(QString a) {
     qDebug() << "it`s Vegener`s cipher function\n";
     return QByteArray();
 }
-QByteArray messageToSha1(QString a) {
+
+QByteArray messageToSha1(QString message) {
     qDebug() << "it`s funcition for transform message in sha1 hash\n";
-    return QByteArray();
+    QByteArray byteArray = message.toUtf8();
+
+    QByteArray hash = QCryptographicHash::hash(byteArray, QCryptographicHash::Sha1);
+
+    QByteArray result;
+    result.append("SHA-1 hash (hex): " + hash.toHex() + "\r\n");
+    result.append("Hashing message: " + message.toUtf8() + "\r\n");
+
+    return result;
 }
-QByteArray messageInMusic() {
+
+QByteArray messageInMusic(QString message, QString musicFilePath) {
     qDebug() << "it`s function for hide text message in the music file\n";
-    return QByteArray();
+    qDebug() << "Hiding message in music file:" << musicFilePath;
+
+    QByteArray messageData = message.toUtf8();
+    quint32 messageSize = messageData.size();
+
+    QFile audioFile(musicFilePath);
+    if (!audioFile.open(QIODevice::ReadWrite)) {
+        return QByteArray("Error: Could not open audio file\r\n");
+    }
+
+    QByteArray audioData = audioFile.readAll();
+
+    if (audioData.size() < 44) {
+        audioFile.close();
+        return QByteArray("Error: Invalid WAV file format\r\n");
+    }
+
+
+    quint32 maxMessageSize = (audioData.size() - 44) / 8;
+    if (messageSize > maxMessageSize) {
+        audioFile.close();
+        return QByteArray("Error: Message too large for this audio file\r\n");
+    }
+
+    for (int i = 0; i < 32; i++) {
+        quint32 pos = 44 + i;
+        if (pos >= audioData.size()) break;
+
+        audioData[pos] = (audioData.at(pos) & 0xFE) | ((messageSize >> (31 - i)) & 1);
+    }
+
+    for (quint32 i = 0; i < messageSize; i++) {
+        for (int bit = 0; bit < 8; bit++) {
+            quint32 pos = 44 + 32 + i * 8 + bit;
+            if (pos >= audioData.size()) break;
+
+            char byte = messageData.at(i);
+            audioData[pos] = (audioData.at(pos) & 0xFE) | ((byte >> (7 - bit)) & 1);
+        }
+    }
+
+    audioFile.seek(0);
+    audioFile.write(audioData);
+    audioFile.close();
+
+    return QByteArray("Message successfully hidden in audio file\r\n");
 }
+
+//декодирование сообщения из музыкального файла
+QString extractMessageFromMusic(QString musicFilePath) {
+    QFile audioFile(musicFilePath);
+    if (!audioFile.open(QIODevice::ReadOnly)) {
+        return "Error: Could not open audio file";
+    }
+
+    QByteArray audioData = audioFile.readAll();
+    audioFile.close();
+
+    if (audioData.size() < 44 + 32) {
+        return "Error: File too small to contain message";
+    }
+
+    quint32 messageSize = 0;
+    for (int i = 0; i < 32; i++) {
+        quint32 pos = 44 + i;
+        if (pos >= audioData.size()) break;
+        messageSize = (messageSize << 1) | (audioData.at(pos) & 1);
+    }
+
+    QByteArray messageData;
+    for (quint32 i = 0; i < messageSize; i++) {
+        char byte = 0;
+        for (int bit = 0; bit < 8; bit++) {
+            quint32 pos = 44 + 32 + i * 8 + bit;
+            if (pos >= audioData.size()) break;
+            byte = (byte << 1) | (audioData.at(pos) & 1);
+        }
+        messageData.append(byte);
+    }
+
+    return QString::fromUtf8(messageData);
+}
+
 QString Encrypt(QString a, QString b , QString c) {
     qDebug() << "it`s ecnrypt funcition\n";
     return QByteArray();
@@ -106,9 +198,13 @@ QByteArray queryAnalyzer(QString message) {
         {
             return messageToSha1(parts.at(1));
         }
-        else if (parts.at(0) == "music" && parts.length() > 0)
+        else if (parts.at(0) == "music" && parts.length() > 2)
         {
-            return messageInMusic();
+            return messageInMusic(parts.at(1), parts.at(2));
+        }
+        //декодирование сообщения из музыкального файла
+        else if (parts.at(0) == "extract" && parts.length() > 1) {
+            return extractMessageFromMusic(parts.at(1)).toUtf8();
         }
         else if (parts.at(0) == "newton" && parts.length() > 0)
         {
--- serverfunctions.h
+++ serverfunctions.h
@@ -6,6 +6,7 @@
 #include <QStringList>
 #include <QByteArray>
 #include <database.h>
+#include <QCryptographicHash>
 
 QByteArray queryAnalyzer(QString);
 QByteArray authentication(QString, QString);
@@ -13,7 +14,7 @@ QByteArray registration(QString, QString, QString);
 QByteArray lookallstat(QString, QString);
 QByteArray vigenereCipher(QString);
 QByteArray messageToSha1(QString);
-QByteArray messageInMusic();
+QByteArray messageInMusic(QString, QString);
 QString Encrypt(QString, QString);
 QString Decrypt(QString, QString, QString);
 double rootByNewton();
