1fedf72aaf5726f41021e3d31873032bbdf9f061
whyflatwhite <whywhysunflower@gmail.com>
2025-06-06 02:51:56 +0300

    Update questionwindow.h
    doxygen.

--- gui/questionwindow.h
+++ gui/questionwindow.h
@@ -6,21 +6,41 @@
 #include <QPushButton>
 #include "client.h"
 
+
+/**
+ * @class QuestionWindow
+ * @brief Класс окна для отображения и ответа на вопрос
+ *
+ * Окно отображает вопрос в зависимости от переданного идентификатора
+ * и предоставляет интерфейс для ввода ответа пользователем.
+ */
 class QuestionWindow : public QWidget {
     Q_OBJECT
 public:
+    /**
+     * @brief Конструктор класса QuestionWindow
+     * @param client Указатель на объект Client для связи с сервером
+     * @param questionId Идентификатор вопроса (1-4)
+     * @param parent Родительский виджет (по умолчанию nullptr)
+     */
     explicit QuestionWindow(Client *client, int questionId, QWidget *parent = nullptr);
 
 signals:
+    /**
+     * @brief Сигнал возврата к окну задач
+     */
     void backToTasks();
 
 private slots:
+    /**
+     * @brief Обработчик нажатия кнопки отправки ответа
+     */
     void onSubmitClicked();
 
 private:
-    Client *client;
-    int questionId;
-    QLineEdit *answerEdit;
+    Client *client;         ///< Указатель на объект Client для связи с сервером
+    int questionId;         ///< Идентификатор текущего вопроса
+    QLineEdit *answerEdit;  ///< Поле для ввода ответа
 };
 
 #endif // QUESTIONWINDOW_H
