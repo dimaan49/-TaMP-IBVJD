ec1e5b71acc87a4d00bcac315bca18ada6eb8bc2
whyflatwhite <whywhysunflower@gmail.com>
2025-06-06 01:51:03 +0300

    Update relations.h
    doxygen

--- relations.h
+++ relations.h
@@ -2,13 +2,31 @@
 #define RELATION
 #include <QString>
 
+/**
+ * @brief Структура, представляющая пользователя системы
+ * 
+ * Содержит основную информацию о пользователе: идентификатор,
+ * имя, пароль и электронную почту
+ */
 struct User {
+    /**
+     * @brief Конструктор по умолчанию
+     * 
+     * Создает пользователя с пустыми полями и id = -1
+     */
     User ();
+    /**
+     * @brief Конструктор с параметрами
+     * @param id Идентификатор пользователя
+     * @param name Имя пользователя
+     * @param password Пароль пользователя
+     * @param email Электронная почта пользователя
+     */
     User (int, QString, QString, QString);
-    int id;
-    QString name;
-    QString password;
-    QString email;
+    int id;         ///< Идентификатор пользователя
+    QString name;    ///< Имя пользователя
+    QString password; ///< Пароль пользователя
+    QString email;    ///< Электронная почта пользователя
 };
 
 
