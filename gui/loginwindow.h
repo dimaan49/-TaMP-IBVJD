066cb5c4888d66e87c0f054b7c9c0a1931d31099
whyflatwhite <whywhysunflower@gmail.com>
2025-06-06 03:11:07 +0300

    Update loginwindow.h
    doxygen.

--- gui/loginwindow.h
+++ gui/loginwindow.h
@@ -7,24 +7,56 @@
 #include "../client/client.h"
 #include <QPalette>
 
+
+/**
+ * @class LoginWindow
+ * @brief Класс окна авторизации пользователя
+ *
+ * Обеспечивает интерфейс для ввода учетных данных (логин/пароль),
+ * выполняет базовую валидацию и взаимодействует с сервером через Client.
+ */
 class LoginWindow : public QWidget {
     Q_OBJECT
 public:
+    /**
+     * @brief Конструктор LoginWindow
+     * @param client Указатель на объект Client для сетевого взаимодействия
+     * @param parent Родительский виджет
+     */
     explicit LoginWindow(Client *client, QWidget *parent = nullptr);
 
 signals:
+    /**
+     * @brief Сигнал успешной авторизации
+     */
     void loginSuccess();
+    
+    /**
+     * @brief Сигнал запроса открытия окна регистрации
+     */
     void openRegister();
 
 private slots:
+    /**
+     * @brief Слот обработки нажатия кнопки входа
+     */
     void onLoginClicked();
+    
+    /**
+     * @brief Слот обработки нажатия кнопки регистрации
+     */
     void onRegisterClicked();
-    void handleServerResponse(const QJsonObject &response);  // Объявлен один раз
+   
+    /**
+     * @brief Слот обработки ответа сервера
+     * @param response JSON-объект с ответом сервера
+     */
+    void handleServerResponse(const QJsonObject &response);
 
 private:
-    QLineEdit *loginEdit;
-    QLineEdit *passwordEdit;
-    Client *client;
+    QLineEdit *loginEdit;    ///< Поле ввода логина
+    QLineEdit *passwordEdit; ///< Поле ввода пароля
+    Client *client;          ///< Указатель на объект Client
 };
 
 #endif // LOGINWINDOW_H
