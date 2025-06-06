5083c989b6e108e5aee93407a04e31b1be31a74c
whyflatwhite <whywhysunflower@gmail.com>
2025-06-06 03:05:53 +0300

    Update mainwindow.h
    doxygen.

--- gui/mainwindow.h
+++ gui/mainwindow.h
@@ -8,25 +8,65 @@
 #include "questionwindow.h"
 #include "client.h"
 
+
+/**
+ * @class MainWindow
+ * @brief Класс главного окна приложения, использующий QStackedWidget
+ *
+ * Управляет переключением между различными экранами приложения:
+ * - Окно входа (LoginWindow)
+ * - Окно регистрации (RegisterWindow)
+ * - Окно задач (TaskWindow)
+ * - Окно вопроса (QuestionWindow)
+ */
 class MainWindow : public QStackedWidget {
     Q_OBJECT
 public:
+    /**
+     * @brief Конструктор MainWindow
+     * @param client Указатель на объект Client для сетевого взаимодействия
+     * @param parent Родительский виджет
+     */
     explicit MainWindow(Client *client, QWidget *parent = nullptr);
 
 private slots:
+    /**
+     * @brief Обработчик успешного входа
+     */
     void onLoginSuccess();
+    
+    /**
+     * @brief Обработчик перехода к окну регистрации
+     */
     void onOpenRegister();
+    
+    /**
+     * @brief Обработчик возврата к окну входа
+     */
     void onBackToLogin();
+    
+    /**
+     * @brief Обработчик отображения окна вопроса
+     * @param id Идентификатор вопроса
+     */
     void onShowQuestion(int id);
+    
+    /**
+     * @brief Обработчик отображения статистики
+     */
     void onShowStats();
+    
+    /**
+     * @brief Обработчик выхода из приложения
+     */
     void onExit();
 
 private:
-    Client *client;
-    LoginWindow *loginWindow;
-    RegisterWindow *registerWindow;
-    TaskWindow *taskWindow;
-    QuestionWindow *questionWindow;
+    Client *client;                 ///< Указатель на клиент для сетевого взаимодействия
+    LoginWindow *loginWindow;       ///< Указатель на окно входа
+    RegisterWindow *registerWindow; ///< Указатель на окно регистрации
+    TaskWindow *taskWindow;         ///< Указатель на окно задач
+    QuestionWindow *questionWindow; ///< Указатель на окно вопроса (создается динамически)
 };
 
 #endif // MAINWINDOW_H
